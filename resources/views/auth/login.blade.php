9deb4cdfc0b8584a476f10745ad406940561c8ee
aldmf26 <aldimf26@gmail.com>
2023-05-03 08:50:46 +0800

    3

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -115,7 +115,7 @@
                     </div>
                 </form>
 
-                <div class="login100-more" style="background-image: url(/assets/login/img/log2.svg);">
+                <div class="login100-more" style="background-image: url(/assets/login/img/login.jpg);">
                 </div>
             </div>
         </div>
